4d1d21ba2733f960ad2b74fdaff37bb4d3b5f475
Stefan Zaharie <stefan@zaha.tech>
2024-01-25 11:22:34 +0200

    Initial commit

---
+++ API/Models/Placeholder.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Placeholder
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string Name { get; set; } = "Placeholder";
+    }
+}
---
+++ API/Models/PlaceholderContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class PlaceholderContext : DbContext
+    {
+        public PlaceholderContext(DbContextOptions<PlaceholderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Placeholder> Placeholder { get; set; }
+    }
+}
